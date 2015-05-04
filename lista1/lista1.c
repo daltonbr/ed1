beeb1724af88a4b03665603e7d6974e765d94db9
daltonbr <sacinopatinete@gmail.com>
2015-05-04 18:27:58 -0300

    esboço da primeira lista pra praticar

---
+++ lista1/lista1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#define MAX 100  //constante de tamanho maximo dalista
+
+struct node {
+    int info, next;
+    struct node Node[MAX];
+}
+
+int menu()
+{
+    int saida = 0;
+    
+    //draw menu
+    
+    return saida;
+}
+
+
+int main ()
+{
+ 	printf("Lista de dados\n");
+    
+
+
+ 	return 0;
+}
