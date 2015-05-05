36460186de06af6fdbcefe51c382e080c0f0d664
daltonbr <sacinopatinete@gmail.com>
2015-05-05 18:50:25 -0300

    Add some basic functions

--- lista1/lista1.c
+++ lista1/lista1.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
 
-#define MAX 100  //constante de tamanho maximo dalista
+#define MAX 100  //constante de tamanho maximo da lista
 
-struct node {
-    int info, next;
-    struct node Node[MAX];
+typedef struct 
+{
+	char nome[30]
+    int idade;
+}ALUNO;
+
+typedef struct 
+{
+	ALUNO vetor[MAX];
+	int nElem;
+}LISTA;
+
+void criarLista (LISTA * lista)
+{
+	lista->nElem = 0;
+}
+
+void inserirFinal (LISTA * lista, CLIENTE cl)
+{
+	lista->vetor[lista->nElem] = cl;
+	lista->nElem ++;
 }
 
 int menu()
@@ -20,8 +38,9 @@ int menu()
 int main ()
 {
  	printf("Lista de dados\n");
-    
-
+  
+  LISTA lista;
+  criarLista (&lista);
 
  	return 0;
 }
